1cb26a7fcc9958f519fb2db4be54891b8a8bbdef
liu930115/ <1270926235@qq.com>
2017-06-28 14:41:29 +0800

    sss

--- app/src/main/java/cn/devbao/com/daggerretrofitdemo/ui/activity/MainActivity.java
+++ app/src/main/java/cn/devbao/com/daggerretrofitdemo/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.devbao.com.daggerretrofitdemo;
+package cn.devbao.com.daggerretrofitdemo.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.devbao.com.daggerretrofitdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
